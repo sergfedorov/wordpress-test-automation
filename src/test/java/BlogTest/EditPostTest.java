2b553608a009e3336be305b939f0f658a557a97d
fedorserg89@mail.ru <fedorserg89@mail.ru>
2016-05-12 15:01:55 +0300

    Create Edit Post test

--- src/test/java/BlogTest/EditPostTest.java
+++ src/test/java/BlogTest/EditPostTest.java
@@ -12,7 +12,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 
-public class CreateNewPostTest {
+public class EditPostTest {
+
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -20,15 +21,23 @@ public class CreateNewPostTest {
         LoginPage loginPg = PageFactory.initElements(driver, LoginPage.class);
         loginPg.login();
 
-        PostsPage postsPg1 = PageFactory.initElements(driver, PostsPage.class);
-        System.out.println(postsPg1.numberOfPosts());
+        String actualTilte = "title update qwerty123";
+        String actualDescription = "description update";
 
         PostsPage postsPg = PageFactory.initElements(driver, PostsPage.class);
-        postsPg.createNewPost("qwerty", "asdafaf");
+        postsPg.editFirstPost(actualTilte, actualDescription);
 
-        PostsPage postsPg2 = PageFactory.initElements(driver, PostsPage.class);
-        postsPg2.deleteFirstPost();
+        driver.findElement(By.cssSelector(".notice__action>span")).click();
 
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+
+        ViewPostPage viewPg = PageFactory.initElements(driver, ViewPostPage.class);
+        viewPg.testTitle(actualTilte);
+        viewPg.testDescription(actualDescription);
 
     }
+    
 }
