f629607d82882946dc09312b955856196dde75d9
Sergey Fedorov <fedorserg89@mail.ru>
2017-01-16 22:37:23 +0200

    polygon change

--- src/test/java/tmp/PolygonV2.java
+++ src/test/java/tmp/PolygonV2.java
@@ -1,9 +1,13 @@
-package tmp;
+package luxtest;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
@@ -11,84 +15,89 @@ import static java.lang.Math.sqrt;
 public class PolygonV2 {
 
     public static void main(String args[]) throws IOException {
-        File file = new File("D:\\point.txt");
-        readFile(file);
+        String inputFilePath = "D:\\point.txt";
+        String outputFilePath = "D:\\output.txt";
+        File inputFile = new File(inputFilePath);
+        File outputFile = new File(outputFilePath);
+
+        readFile(inputFile, outputFile);
     }
 
-    private static void readFile(File fin) throws IOException {
+    private static void readFile(File inFile, File outFile) throws IOException {
+        FileOutputStream fos = new FileOutputStream(outFile);
+        FileInputStream fis = new FileInputStream(inFile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
-        String FILENAME = "D:\\output.txt";
-        BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME));
+        ArrayList<String> coordinatePairArrayFromLine;
+        ArrayList<ArrayList<String>> coordinatePairArrayFromFile = new ArrayList<ArrayList<String>>();
+        ArrayList<Integer> singleCoordinateArrayFromLine;
+        ArrayList<ArrayList<Integer>> singleCoordinateArrayFromFile = new ArrayList<ArrayList<Integer>>();
 
-        FileInputStream fis1 = new FileInputStream(fin);
-        BufferedReader br1 = new BufferedReader(new InputStreamReader(fis1));
-
-        int lineNumber = 1;
         String line;
+        int lineNumber = 1;
 
-        ArrayList<ArrayList<Integer>> myList = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> row;
+        while ((line = br.readLine()) != null) {
 
-        ArrayList<ArrayList<String>> lineList = new ArrayList<ArrayList<String>>();
-        ArrayList<String> lineRow;
-
-        while ((line = br1.readLine()) != null) {
-
-            System.out.println("Coordinate number: " + lineNumber);
-            //validation
+            System.out.println("Line #" + lineNumber + " is " + line);
+            //Validation
             String regex = "([0-9]+:[0-9]+,){1,}[0-9]+:[0-9]+|[0-9]+:[0-9]+";
             if (line.matches(regex)) {
-                System.out.println("valid coordinates");
+                System.out.println("--- Validation is PASSED");
             } else {
-                System.out.println("invalid coordinates. Please use the pattern: for one coordinate: X1:Y1 or for two or more coordinates: X1:Y1,X2:Y2,X3:Y3");
-                System.out.println(line);
+                System.out.println("--- Validation is FAILED. Moving to the next line.");
                 lineNumber++;
                 continue;
             }
 
-            String[] ln = line.split(",");
+            //System.out.
 
-            int t = 0;
-            lineRow = new ArrayList<String>();
-            row = new ArrayList<Integer>();
+            String[] coordinatePair = line.split(",");
 
-            for (int stolb = 0; stolb < ln.length; stolb++) {
-                lineRow.add(ln[stolb]);
-                String[] ln1 = ln[stolb].split(":");
+            int n = 0;
+            coordinatePairArrayFromLine = new ArrayList<String>();
+            singleCoordinateArrayFromLine = new ArrayList<Integer>();
 
-                for (int ss = 0; ss < ln1.length; ss++) {
+            // Loop for each coordinate pair. coordinatePairArray contains all coordinate pairs from current file line
+            for (int cp = 0; cp < coordinatePair.length; cp++) {
+                coordinatePairArrayFromLine.add(coordinatePair[cp]);
 
-                    row.add(t, Integer.parseInt(ln1[ss]));
-                    t++;
+                String[] singleCoordinate = coordinatePair[cp].split(":");
+
+                // Loop for single coordinate. singleCoordinateArray contains all coordinates from current file line
+                for (int sc = 0; sc < singleCoordinate.length; sc++) {
+                    singleCoordinateArrayFromLine.add(n, Integer.parseInt(singleCoordinate[sc]));
+                    n++;
                 }
 
             }
 
-            bw.write(String.valueOf(row));
+            bw.write(String.valueOf(singleCoordinateArrayFromLine));
 
-            myList.add(row);
-            lineList.add(lineRow);
+            singleCoordinateArrayFromFile.add(singleCoordinateArrayFromLine);
+            coordinatePairArrayFromFile.add(coordinatePairArrayFromLine);
 
-            //check each line from the file
-            whatTheFigureIs(row);
+            //Checks
+            whatTheFigureIs(singleCoordinateArrayFromLine);
 
             lineNumber++;
 
         }
 
-        System.out.println(myList);
-        System.out.println(lineList);
-        sortAndPrintMultidimArrayList(lineList);
+        //System.out.println(singleCoordinateArrayFromFile);
+        System.out.println(coordinatePairArrayFromFile);
+        //sortAndPrintMultidimArrayList(coordinatePairArrayFromFile);
 
-        br1.close();
+        br.close();
         bw.close();
-    }
-
-    public static void lineValidator(String line){
 
     }
 
-    public static void sortAndPrintMultidimArrayList(ArrayList list){
+    public static void lineValidator(String line) {
+
+    }
+
+    public static void sortAndPrintMultidimArrayList(ArrayList list) {
         //sort by vertex number
         Collections.sort(list, new Comparator<ArrayList<String>>() {
             @Override
@@ -97,41 +106,44 @@ public class PolygonV2 {
             }
         });
         //print the coordinates
-        for(Object singleLine : list){
+        for (Object singleLine : list) {
             System.out.println(singleLine);
         }
     }
 
-    public static void whatTheFigureIs(ArrayList list){
-        if(list.size() == 2){
-            System.out.println("Point");
-        } else if(list.size() == 4){
+    public static void whatTheFigureIs(ArrayList list) {
+        if (list.size() == 2) {
+            System.out.println("--- RESULT: This is a POINT");
+        } else if (list.size() == 4) {
             //line
-            isLineCorrect(list);
-        } else if(list.size() == 6){
+            System.out.println(isLineCorrect(list));
+        } else if (list.size() == 6) {
             //triangle
-            System.out.println("Triangle is good - " + isTriangleCorrectByCoordinates(list));
-        } else if(list.size() == 8){
+            System.out.println(isTriangleCorrectByCoordinates(list));
+        } else if (list.size() == 8) {
             System.out.println("It is square - " + isItSquare(list));
             System.out.println("4angle");
-        } else if(list.size() > 8){
+        } else if (list.size() > 8) {
             //manyangle
         }
     }
 
-    public static void isLineCorrect(ArrayList list){
+    public static String isLineCorrect(ArrayList list) {
         if (list.get(0).equals(list.get(2)) && list.get(1).equals(list.get(3)))
-            System.out.println("Invalid line");
+            return "--- RESULT: This is an INVALID LINE. Both coordinate pairs are identical";
         else
-            System.out.println("Valid line");
+            return "--- RESULT: This is a LINE";
     }
 
-    public static boolean isTriangleCorrectByCoordinates(ArrayList<Integer> list) {
+    public static String isTriangleCorrectByCoordinates(ArrayList<Integer> list) {
         double sideA = sqrt(pow((list.get(1) - list.get(0)), 2) + pow((list.get(3) - list.get(2)), 2));
         double sideB = sqrt(pow((list.get(3) - list.get(2)), 2) + pow((list.get(5) - list.get(4)), 2));
         double sideC = sqrt(pow((list.get(5) - list.get(4)), 2) + pow((list.get(1) - list.get(0)), 2));
-        return ((sideA < sideB + sideC) && (sideA > sideB - sideC) && (sideB < sideA + sideC) && (sideB > sideA - sideC) && (sideC < sideA + sideB) && (sideC > sideA - sideB));
-
+        boolean res = ((sideA < sideB + sideC) && (sideA > sideB - sideC) && (sideB < sideA + sideC) && (sideB > sideA - sideC) && (sideC < sideA + sideB) && (sideC > sideA - sideB));
+        if (res)
+            return "--- RESULT: This is a TRIANGLE";
+        else
+            return "--- RESULT: This is an INVALID TRIANGLE. Please check coordinates";
     }
 
     public static boolean isItSquare(ArrayList<Integer> list) {
